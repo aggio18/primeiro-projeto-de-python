0877e71512f150d6929ac137b5a2c491da51b6df
Pedro Aggio <aggiopedro14@gmail.com>
2026-08-29 11:12:52 -0300

    advinhe o numero loop
    adicionei um loop ao programa

--- teste.py
+++ teste.py
@@ -1,19 +1,25 @@
-menor = 1
-maior = 100
-tentativas = 0
+while True :
+    menor = 1
+    maior = 100
+    tentativas = 0
 
-while True: 
-    palpite = (menor + maior) // 2
-    tentativas = tentativas + 1
-    resposta = input(f"Seu numero e {palpite}? (digite 'acertou', 'maior', 'menor'):").lower()
-    if resposta == "acertou":   
-        print(f"Consegui! Advinhei em {tentativas} tentativas!")
-        break
-    elif resposta == "maior":
-            menor = palpite + 1
+    while True: 
+        palpite = (menor + maior) // 2
+        tentativas = tentativas + 1
+        resposta = input(f"Seu numero e {palpite}? (digite 'acertou', 'maior', 'menor'):").lower()
+        if resposta == "acertou":   
+            print(f"Consegui! Advinhei em {tentativas} tentativas!")
+            break
+        elif resposta == "maior":
+                menor = palpite + 1
+                print(f"Nao consegui tentarei outro numero")
+        elif resposta == "menor":
+            maior = palpite -1
             print(f"Nao consegui tentarei outro numero")
-    elif resposta == "menor":
-        maior = palpite -1
-        print(f"Nao consegui tentarei outro numero")
-    else:
-        print("Opcao invalida! Digite apenas 'acertou', 'maior', 'menor'. ")
+        else:
+            print("Opcao invalida! Digite apenas 'acertou', 'maior', 'menor'. ")
+
+# Pergunta pos-jogo (fora da partida, dentro do loop principal)
+    jogar_de_novo = input("Quer jogar de novo? (S/N):").lower()
+    if jogar_de_novo == "n":    
+         break #Encerra o programa
