2e32da529e3af3da2c44587600d4083ad66436af
Pedro Aggio <aggiopedro14@gmail.com>
2026-08-29 10:51:24 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,19 @@
+menor = 1
+maior = 100
+tentativas = 0
+
+while True: 
+    palpite = (menor + maior) // 2
+    tentativas = tentativas + 1
+    resposta = input(f"Seu numero e {palpite}? (digite 'acertou', 'maior', 'menor'):").lower()
+    if resposta == "acertou":   
+        print(f"Consegui! Advinhei em {tentativas} tentativas!")
+        break
+    elif resposta == "maior":
+            menor = palpite + 1
+            print(f"Nao consegui tentarei outro numero")
+    elif resposta == "menor":
+        maior = palpite -1
+        print(f"Nao consegui tentarei outro numero")
+    else:
+        print("Opcao invalida! Digite apenas 'acertou', 'maior', 'menor'. ")
